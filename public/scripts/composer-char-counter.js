5ee4893b994be5ae836599ebba128a01d7128216
Matthew Davis <matc.davis@gmail.com>
2022-08-31 18:12:01 +0000

    write jQuery which displays character count left on the tweet form

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-  
+  console.log(document);
+
+ 
+
+  $("#tweet-text").on('input', function() {
+    // console.log(this);
+    const charCount = $(this).val().length
+    const charLeft = 140 - charCount;
+    if (charLeft <= 0) {
+      
+    }
+    $(this).siblings().children(".counter").html(charLeft)
+
+
+  });
+
 })
+
