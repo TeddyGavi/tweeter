5c1ca5c78645f0d81c9c744fe4a938b6a81f995e
Matthew Davis <matc.davis@gmail.com>
2022-09-02 22:44:53 +0000

    write error check for submit event, modify css to display long tweets by wrapping

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,29 +6,42 @@
 
 $(document).ready(function () {
 
-    $("#tweet-form").submit(function(e) {
+  $("#tweet-form").submit(function (e) {
     e.preventDefault();
+    e.stopPropagation();
     const data = $(this).serialize();
-    $.post("/tweets", data, () => {
-      console.log("done appended DB");
-    })
+    const charCount = $(this).find(".counter").html()
+    if (Number(charCount) === 140) {
+      alert("Tweet cannot be empty")
+      return
+    } else if (charCount < 0) {
+      alert("Character limit reached! please shorten your tweet")
+      return
+    } else {
+      $.post("/tweets", data, () => {
+        console.log("done appended DB", data, charCount);
+        $("#tweet-form").trigger("reset")
+        $(this).find(".counter").html(140);
+        loadTweets();
+      })
+    }
   })
 
   const loadTweets = () => {
-    $.get("/tweets", function(data) {
+    $.get("/tweets", function (data) {
       renderTweet(data)
     })
   }
-/* wrote my own function before using timeAgo
-  const getDaysAgo = (timeStamp) => {
-    //timestamp must be in milliseconds
-    //need to add a check for this possibly?
-    const timeStampNow = new Date().getTime();
-    const diffInUnix = timeStampNow - timeStamp;
-    const diffInDays = Math.floor(diffInUnix / 1000 / 60 / 60 / 24);
-
-    return diffInDays;
-  }; */
+  /* wrote my own function before using timeAgo
+    const getDaysAgo = (timeStamp) => {
+      //timestamp must be in milliseconds
+      //need to add a check for this possibly?
+      const timeStampNow = new Date().getTime();
+      const diffInUnix = timeStampNow - timeStamp;
+      const diffInDays = Math.floor(diffInUnix / 1000 / 60 / 60 / 24);
+  
+      return diffInDays;
+    }; */
 
   const renderTweet = (db) => {
     db.forEach(e => $(".tweet-container").append(createTweetElement(e)));
